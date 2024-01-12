91d74ffc8f420b58c009bf76d974042560fba009
LarsG-2158052 <lars.gielen@student.uhasselt.be>
2024-01-12 17:34:13 +0100

    Add Tuner UI to website

---
+++ Main-MusicHub/public/js/Tuner/TunerView.js
@@ -0,0 +1,104 @@
+var deviceNameInput = null;
+var setDeviceBtn = null;
+var noteOutput = null;
+var pitchOutput = null;
+var connectedDeviceOutput = null;
+
+var client = null;
+
+const brokerUrl = 'wss://c3a83306c2b24c68835c34e6983a57b1.s2.eu.hivemq.cloud:8884/mqtt';
+
+window.onload = function() {
+    deviceNameInput = document.querySelector("#deviceNameInput");
+    setDeviceBtn = document.querySelector("#setDeviceBtn");
+    noteOutput = document.querySelector("#noteOutput");
+    pitchOutput = document.querySelector("#pitchOutput");
+    connectedDeviceOutput = document.querySelector("#connectedDeviceOutput");
+
+    client = new Paho.MQTT.Client(brokerUrl, 'webClient');
+
+    setDeviceBtn.onclick = onSetDeviceBtn;
+
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    client.connect({
+        onSuccess: onConnect,
+        onFailure: onFailure,
+        userName: 'Admin',
+        password: 'Admin123',
+    });
+}
+
+function onSetDeviceBtn() {
+    client.unsubscribe('#');
+    connectedDeviceOutput.innerHTML = "";
+    noteOutput.innerHTML = "";
+    pitchOutput.innerHTML = "";
+
+    if (client == null)
+        return;
+
+    if (!deviceNameInput.value || deviceNameInput.value.trim() === "")
+        return;
+    
+    client.subscribe(deviceNameInput.value);
+    connectedDeviceOutput.innerHTML = "Subscribed to: " + deviceNameInput.value;
+}
+
+function onMessageArrived(message) {
+    let dataArray = message.payloadString.split("|");
+
+    let noteValue = dataArray[0];
+    let pitchValue = dataArray[1];
+
+    noteOutput.innerHTML = noteValue;
+    pitchOutput.innerHTML = pitchValue;
+
+    if (pitchValue < 0)
+        pitchOutput.innerHTML = "Higher";
+    else if (pitchValue > 0)
+        pitchOutput.innerHTML = "Lower";
+    else
+        pitchOutput.innerHTML = "";
+
+    var color = [0, 0, 0];
+    if (Math.abs(pitchValue) <= 10){
+        colorGoodPitch = [0, 255, 0];
+        colorBadPitch = [255, 165, 0];
+
+        color = colorGoodPitch.map((start, i) => {
+            const end = colorBadPitch[i];
+            const delta = end - start;
+            return Math.round(start + delta * Math.min(Math.max(Math.abs(pitchValue) / 10, 0), 1));
+        });
+    }
+    else {
+        var colorGoodPitch = [255, 165, 0];
+        var colorBadPitch = [255, 0, 0];
+
+        color = colorGoodPitch.map((start, i) => {
+            const end = colorBadPitch[i];
+            const delta = end - start;
+            return Math.round(start + delta * Math.min(Math.max(Math.abs(pitchValue - 10) / 40, 0), 1));
+        });
+    }
+
+    noteOutput.style.color = `rgb(${color.join(", ")})`;
+}
+
+function onConnect() {
+    console.log('Connected to MQTT broker');
+}
+
+function onFailure(err) {
+    console.log('Connection failed: ' + err.errorMessage);
+    client = null;
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log('Connection lost: ' + responseObject.errorMessage);
+        client = null;
+    }
+}
--- Main-MusicHub/resources/views/Tuner/tuner.blade.php
+++ Main-MusicHub/resources/views/Tuner/tuner.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
+    <script src="{{ asset('/js/Tuner/TunerView.js') }}"></script>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ "Tuner" }}
@@ -9,10 +13,14 @@
         <x-input-label for="deviceNameInput" value="Device name:" class="text-xl"/>
         <div class="flex gap-4">
             <x-text-input id="deviceNameInput" class="w-full" type="text"></x-text-input>
-            <x-secondary-button id="joinBtn">{{ "Set" }}</x-secondary-button>
+            <x-secondary-button id="setDeviceBtn">{{ "Subscribe" }}</x-secondary-button>
         </div>
+        <p id="connectedDeviceOutput"></p>
     </div>
 
-    <p class="w-full text-9xl my-52 text-center">G</p>
+    <div class="my-40">
+        <p id="noteOutput" class="w-full text-9xl text-center"></p>
+        <p id="pitchOutput" class="w-full text-5xl text-center"></p>
+    </div>
 
 </x-app-layout>
