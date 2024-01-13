c2a76030225ff27c1aac687d41099eef72162639
LarsG-2158052 <lars.gielen@student.uhasselt.be>
2024-01-13 20:28:36 +0100

    Add Dockerfile

--- GraphQL-InstrumentLibraryAPI/src/server.js
+++ GraphQL-InstrumentLibraryAPI/src/server.js
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 });
   
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: process.env.PORT || 4000 },
 })
 .then(({ url }) => {
   console.log(`🚀  Server ready at: ${url}`);
